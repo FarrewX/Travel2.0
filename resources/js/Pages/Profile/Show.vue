92e3f791b7fe725509e406a0d980fb61f5a38a8b
FarrewX <a0987142276@gmail.com>
2024-02-27 01:08:33 +0700

    bug edit profile

--- resources/js/Pages/Profile/Show.vue
+++ resources/js/Pages/Profile/Show.vue
@@ -1,5 +1,6 @@
 <script setup>
 import AppLayout from '@/Layouts/AppLayout.vue';
+import { Head, Link, router } from '@inertiajs/vue3';
 import DeleteUserForm from '@/Pages/Profile/Partials/DeleteUserForm.vue';
 import LogoutOtherBrowserSessionsForm from '@/Pages/Profile/Partials/LogoutOtherBrowserSessionsForm.vue';
 import SectionBorder from '@/Components/SectionBorder.vue';
@@ -14,12 +15,14 @@ defineProps({
 </script>
 
 <template>
-    <AppLayout title="Profile">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Profile
+    <AppLayout></AppLayout>
+    <Head title="Profile"></Head>
+        <div> <br>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight"
+            style="text-align: center;">
+                Edit Profile
             </h2>
-        </template>
+        </div>
 
         <div>
             <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
@@ -53,5 +56,5 @@ defineProps({
                 </template>
             </div>
         </div>
-    </AppLayout>
+    
 </template>
